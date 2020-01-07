041deecaaa66ec3ecc09f6f9c3f8dab123a7f37e
nirb <nir.b@investing.com>
2020-01-07 22:40:20 +0200

    Scroll To Bottom

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,16 +1,21 @@
-import React from 'react';
-// import ScrollToBottom from 'react-scroll-to-bottom';
+import React, { useEffect, createRef } from 'react';
 import "./MessageList.scss";
 import Message from "./Message/Message";
 
 const MessageList = ({ messages, username }) => {
 
+	const messageListRef = createRef();
+
 	const renderMessages = () => {
 		return messages.map((message, i) => <Message message={message} username={username} key={i}/>);
 	}
 
+	useEffect(() => {
+		messageListRef.current.scrollTo({ top: messageListRef.current.scrollHeight, behavior: 'smooth' }); // Scroll to bottom if needed upon new messages
+	}, [messages]);
+
 	return (
-		<main className="chat-messages">
+		<main className="chat-messages" ref={messageListRef}>
 			<ol className="chat-messages-list">
 				{renderMessages()}
 			</ol>
